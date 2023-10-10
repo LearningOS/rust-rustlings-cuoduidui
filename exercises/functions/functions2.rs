c6259f8ca66534044082f11bd7ab001116ec90b9
yangfengshan2 <yangfengshan2@jd.com>
2023-10-10 17:09:06 +0800

    练习

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -1,13 +1,12 @@
 // functions2.rs
 // Execute `rustlings hint functions2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     call_me(3);
 }
-
-fn call_me(num:) {
+// rust 基本类型 https://blog.csdn.net/qq_21484461/article/details/131445816
+fn call_me(num:u32) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,12 @@
 // variables5.rs
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
+    // number = 3; // don't rename this variable
+    // Rust有类型检查,执行运算或者赋值时候要遵循类型的规律,但是Rust可以重新定义同名变量,变量的类型可以发生改变
+    let number = 3;
     println!("Number plus two is : {}", number + 2);
 }
